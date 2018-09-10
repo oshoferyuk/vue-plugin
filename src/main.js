1c78cf302573b9f554b02eac0e9f132d40966ec9
ashoferyuk <a.shoferyuk@intetics.com>
2018-09-10 18:40:45 +0300

    initial plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,45 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+
+// const myPlugin = (Vue) => {}
+// const myPlugin = { install(Vue){}}
+// this.$options - whole options
+
+const RulesPlugin = (Vue) => {
+
+  Vue.mixin({
+    created(){
+      if(this.$options.hasOwnProperty('rules')){
+        const rules = this.$options.rules;
+
+        Object.keys(rules).forEach(key => {                    
+          const rule = rules[key];
+          this.$watch(key, newValue => {  
+            const result = rule.validate(newValue)
+            if (!result) {
+              console.log(rule.message)
+            }
+          });
+        })
+      }
+
+    }
+  })
+
+}
+
+Vue.use(RulesPlugin);
+
+const vm = new Vue({
+  data: {foo: 10},
+  rules: {
+    foo: {
+      validate: value => value > 1,
+      message: 'foo must be greater then one'
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
+
+vm.foo = -3;
